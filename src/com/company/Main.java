051c47b17f20446c292cd9529ef6c181f447cba1
Saeed-ld <lari.saeid74@gmail.com>
2021-05-01 23:34:37 +0430

    just added symbols to the password

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,17 +8,28 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String value = "abcdefghijklmnopqrstuvwxyz";
         char[] alphabet = value.toCharArray();
+        String symbol = "#$%_-&";
+        char[] symbolArray = symbol.toCharArray();
 
         while(true){
             System.out.println(" do you want a new password? ");
             String answer = scanner.next();
             if (answer.equals("yes")){
                 String output =
-                        alphabet[(int) (26 * (Math.random()))] + (int) (26 * (Math.random())) + String.valueOf(alphabet[(int) (26 * (Math.random()))]).toUpperCase() +
-                                String.valueOf(alphabet[(int) (26 * (Math.random()))]).toUpperCase() + (int) (26 * (Math.random())) + (int) (26 * (Math.random())) + alphabet[(int) (26 * (Math.random()))] +
-                                alphabet[(int) (26 * (Math.random()))] + String.valueOf(alphabet[(int) (26 * (Math.random()))]).toUpperCase() +
-                                alphabet[(int) (26 * (Math.random()))] + (int) (26 * (Math.random())) + alphabet[(int) (26 * (Math.random()))] +
-                                String.valueOf(alphabet[(int) (26 * (Math.random()))]).toUpperCase() + alphabet[(int) (26 * (Math.random()))];
+                                alphabet[(int) (26 * (Math.random()))]       //letter
+                                + symbolArray[(int) (6 * (Math.random()))]  //symbol
+                                + String.valueOf(alphabet[(int) (26 * (Math.random()))]).toUpperCase()      //uppercase letter
+                                + String.valueOf(alphabet[(int) (26 * (Math.random()))]).toUpperCase()      //uppercase letter
+                                + (int) (26 * (Math.random()))              //number
+                                + (int) (26 * (Math.random()))              //number
+                                + alphabet[(int) (26 * (Math.random()))]    //letter
+                                + symbolArray[(int) (6 * (Math.random()))]  //symbol
+                                + symbolArray[(int) (6 * (Math.random()))]  //symbol
+                                + alphabet[(int) (26 * (Math.random()))]    //letter
+                                + (int) (26 * (Math.random()))              //number
+                                + symbolArray[(int) (6 * (Math.random()))]  //symbol
+                                + String.valueOf(alphabet[(int) (26 * (Math.random()))]).toUpperCase()      //uppercase letter
+                                + alphabet[(int) (26 * (Math.random()))];   //letter
                 char[] randomizer = output.toCharArray();
                 for (int i = 1; i <= 14 ; i++){
                     System.out.print(randomizer[(int) (14 * (Math.random()))]);
