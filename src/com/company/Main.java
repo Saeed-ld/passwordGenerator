100b43f19ae74a98b14aa42c42791f52be2ab64b
Saeed-ld <lari.saeid74@gmail.com>
2021-05-15 07:30:55 +0430

    refactored the code a little

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String value = "abcdefghijklmnopqrstuvwxyz";
-        char[] alphabet = value.toCharArray();
-        String symbol = "#$%_-&";
-        char[] symbolArray = symbol.toCharArray();
 
+        String value = "abcdefghijklmnopqrstuvwxyz";
+        String symbol = "#$%_-&";
+        randomizer(value.toCharArray(), symbol.toCharArray());
+
+    }
+    public static void randomizer(char[] alphabet,char[] symbolArray){
+        Scanner scanner = new Scanner(System.in);
         while(true){
             System.out.println(" do you want a new password? ");
             String answer = scanner.next();
             if (answer.equals("yes")){
                 String output =
-                                alphabet[(int) (25 * (Math.random()))]       //letter
+                        alphabet[(int) (25 * (Math.random()))]       //letter
                                 + symbolArray[(int) (5 * (Math.random()))]  //symbol
                                 + String.valueOf(alphabet[(int) (25 * (Math.random()))]).toUpperCase()      //uppercase letter
                                 + String.valueOf(alphabet[(int) (25 * (Math.random()))]).toUpperCase()      //uppercase letter
